9f3f08c1ccaac232da2c40573f67459b8edd846a
Shitomo <cadaeibf@outlook.com>
2022-12-03 21:40:16 +0900

    chore 不要なファイル削除を止める&apt-getのログを省略& goを直接実行するよう修正

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,21 +1,15 @@
 #!/bin/sh -eu
 
-apt-get install wget
+apt-get -qq install wget
 
 GO_VERSION=1.19.3
 
 GO_TAR=go${GO_VERSION}.linux-amd64.tar.gz
 
-sudo rm -rf /usr/local/go/
-
 wget --quiet https://dl.google.com/go/${GO_TAR}
 
 sudo tar -C /usr/local -xzf ${GO_TAR}
 
-rm -rf ${GO_TAR}
-
-export PATH=$PATH:/usr/local/go
-
-go install go.mozilla.org/sops/v3/cmd/sops@latest
+/usr/local/go install go.mozilla.org/sops/v3/cmd/sops@latest
 
 sops --decrypt --in-place k8s/templates/secret.yaml
