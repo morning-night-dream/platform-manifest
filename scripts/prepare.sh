06036c0e1f5881d1184543a9205e3aa5a5152a08
Shitomo <cadaeibf@outlook.com>
2022-12-03 21:53:07 +0900

    chore /usr/local/go/binにパスを通す

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,6 +10,8 @@ wget --quiet https://dl.google.com/go/${GO_TAR}
 
 sudo tar -C /usr/local -xzf ${GO_TAR}
 
-/usr/local/go install go.mozilla.org/sops/v3/cmd/sops@latest
+export PATH=$PATH:/usr/local/go:/usr/local/go/bin
+
+go install go.mozilla.org/sops/v3/cmd/sops@latest
 
 sops --decrypt --in-place k8s/templates/secret.yaml
