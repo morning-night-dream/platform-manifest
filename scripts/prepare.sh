b630cd32249e2c129a73e0d658196fb977fa920e
Shitomo <cadaeibf@outlook.com>
2022-12-03 21:28:19 +0900

    chore oktetoへのデプロイ時に復号化するよう修正

---
+++ scripts/prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -eu
+
+GO_VERSION=1.19.3
+
+GO_TAR=go${GO_VERSION}.linux-amd64.tar.gz
+
+sudo rm -rf /usr/local/go/
+
+wget https://dl.google.com/go/${GO_TAR}
+
+sudo tar -C /usr/local -xzf ${GO_TAR}
+
+rm -rf ${GO_TAR}
+
+echo PATH=$PATH:/usr/local/go
+
+go install go.mozilla.org/sops/v3/cmd/sops@latest
+
+sops --decrypt --in-place k8s/templates/secret.yaml
