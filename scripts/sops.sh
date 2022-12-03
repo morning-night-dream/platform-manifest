258671b73ab782e059d142e0df4c9c110943f804
Shitomo <cadaeibf@outlook.com>
2022-12-03 22:01:17 +0900

    chore githubのリリースから実行ファイルをダウンロードするよう修正

---
+++ scripts/sops.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -eu
+
+SOPS_VERSION=3.7.3
+
+uname -a
+
+apt-get install wget > /dev/null
+
+wget --quiet https://github.com/mozilla/sops/releases/download/v${SOPS_VERSION}/sops-v${SOPS_VERSION}.linux.amd64
+
+mv sops-v${SOPS_VERSION}.linux.amd64 sops
+
+chmod +x sops
+
+./sops --decrypt --in-place k8s/templates/secret.yaml
