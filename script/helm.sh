42bbd8f882753093a77515d5411abfd13f8f8fcf
takokun <takokun.778@gmail.com>
2022-12-10 17:07:41 +0900

    feat helm.sh

---
+++ script/helm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eu
+
+if [ $(helm list | wc -l) -le 1 ]; then
+  helm install morning-night-dream-platform ./k8s --namespace=fs-morning-night-dreamer
+else
+  helm upgrade morning-night-dream-platform ./k8s --namespace=fs-morning-night-dreamer
+fi
