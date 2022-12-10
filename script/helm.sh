38c382e1419e208bd1e23706f51762cbffa78f49
takokun <takokun.778@gmail.com>
2022-12-10 17:31:23 +0900

    fix helm.sh

--- script/helm.sh
+++ script/helm.sh
@@ -2,7 +2,7 @@
 
 NAME_SPACE=fs-morning-night-dreamer
 
-if [ $(helm list --namespace ${NAME_SPACE} | wc -l) -le 1 ]; then
+if [ $(helm list --namespace "${NAME_SPACE}" | wc -l) -le 1 ]; then
   helm install morning-night-guild-platform ./k8s --namespace=${NAME_SPACE}
 else
   helm upgrade morning-night-guild-platform ./k8s --namespace=${NAME_SPACE}
