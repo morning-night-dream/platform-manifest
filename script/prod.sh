cdaab0061c37850d2b8fbf43210970fed3517aad
takokun <takokun.778@gmail.com>
2022-12-24 14:36:21 +0900

    fix script

--- script/prod.sh
+++ script/prod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eu
 
-helm upgrade --install morning-night-dream-platform ./k8s --namespace=fs-morning-night-dreamer --values ./env/prod.yaml
+helm upgrade --install morning-night-dream-platform ./k8s --namespace=fs-morning-night-dreamer --values ./k8s/env/prod.yaml
